b660c07ab90bf03604866b726bcb2b1a47445d6d
Mohamad Samie <mohamad.samieshad@gmail.com>
2019-08-25 14:20:08 +0430

    anetwork/validation: 2.2 added

--- config/app.php
+++ config/app.php
@@ -161,6 +161,7 @@ return [
         Illuminate\Translation\TranslationServiceProvider::class,
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
+        Anetwork\Validation\PersianValidationServiceProvider::class,
 
         /*
          * Package Service Providers...
